00209cdf79ff21ca5f3b6e96e40a8a64a84283c2
Nikos <ergasies.uni@gmail.com>
2022-02-06 12:15:29 +0100

    change script to function.

--- Matlab/runTest_maxValis4.m
+++ Matlab/runTest_maxValis4.m
@@ -1,12 +1,14 @@
-% runtests.m
-exit_code = 0;
+function runTest_maxValis4()
+    exit_code = 0;
 
-try
-    test_aModel_maxValis4
-catch ME
-    disp(getReport(ME))
-    exit_code = 1;
+    try
+        test_aModel_maxValis4
+    catch ME
+        disp(getReport(ME))
+        exit_code = 1;
+    end
+
+    % Ensure that we ALWAYS call exit
+    exit(exit_code);
+
 end
-
-% Ensure that we ALWAYS call exit
-exit(exit_code);
