a8b29418c72bf5f7f328405149b2bdb592c93e63
Nikolaos Tosis <ergasies.uni@gmail.com>
2022-03-11 07:44:53 +0100

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,7 +119,7 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
